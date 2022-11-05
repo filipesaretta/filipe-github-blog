b74a34db90dc2081cff604505af6feda127e9e80
filipesaretta <filipessm124@gmail.com>
2022-11-05 11:16:25 -0300

    Create post page

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -2,23 +2,24 @@ import { Link } from 'react-router-dom'
 
 export function PostList() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mx-4 pb-8">
-      <div className="bg-base-profile rounded-lg  p-8 hover:outline-none hover:border-base-label hover:ring-base-label hover:ring-2  ease-in-out duration-300 cursor-pointer">
-        <div className="grid grid-cols-[1fr_auto] items-start">
-          <span className="text-xl text-base-title block mb-5">
-            JavaScript data types and data structures
-          </span>
-          <span className="text-sm text-base-span block self-start mt-2">
-            Há 1 dia
-          </span>
+    <Link to={'/post'}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mx-4 pb-8">
+        <div className="bg-base-profile rounded-lg  p-8 hover:outline-none hover:border-base-label hover:ring-base-label hover:ring-2  ease-in-out duration-300 cursor-pointer">
+          <div className="grid grid-cols-[1fr_auto] items-start">
+            <span className="text-xl text-base-title block mb-5">
+              JavaScript data types and data structures
+            </span>
+            <span className="text-sm text-base-span block self-start mt-2">
+              Há 1 dia
+            </span>
+          </div>
+          <p className="text-base-text">
+            Programming languages all have built-in data structures, but these
+            often differ from one language to another. This article attempts to
+            list the built-in data structures available in...
+          </p>
         </div>
-        <p className="text-base-text">
-          Programming languages all have built-in data structures, but these
-          often differ from one language to another. This article attempts to
-          list the built-in data structures available in...
-        </p>
       </div>
-      <Link to={'/post'}>WQOIDDDDDDDDDD</Link>
-    </div>
+    </Link>
   )
 }
