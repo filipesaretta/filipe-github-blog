2c6c2fb2788e47742fe5da3a8aa293366470b505
filipesaretta <filipessm124@gmail.com>
2022-11-08 11:04:20 -0300

    Dynamic routes

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,33 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { api } from '../../lib/axios'
-
-interface PostsProps {
-  title: string
-  created_at: string
-  body: string
-}
+import { PostsContext } from '../../contexts/PostsContext'
 
 export function PostList() {
-  const [posts, setPosts] = useState<PostsProps[]>([])
-
-  async function fetchPosts() {
-    const response = await api.get(
-      '/repos/filipesaretta/filipe-github-blog/issues',
-    )
-    setPosts(response.data)
-  }
-  console.log(posts)
-  useEffect(() => {
-    fetchPosts()
-  }, [])
-
+  const { posts } = useContext(PostsContext)
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mx-4 pb-8">
       {posts.map((post) => {
-        console.log(post.body.slice(0, 100))
         return (
-          <Link to={'/post'} key={post.title}>
+          <Link to={`post/${post.number}`} key={post.title}>
             <div className="bg-base-profile rounded-lg  p-8 hover:outline-none hover:border-base-label hover:ring-base-label hover:ring-2  ease-in-out duration-300 cursor-pointer">
               <div className="grid grid-cols-[1fr_auto] items-start">
                 <span className="text-xl text-base-title block mb-5">
