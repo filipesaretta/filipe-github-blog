e3b2109ec41395c2ae1855cba424e85c487b818c
filipesaretta <filipessm124@gmail.com>
2022-11-08 11:05:09 -0300

    Add react-markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
+import { PostsContextProvider } from './contexts/PostsContext'
 import { Router } from './Routes'
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <PostsContextProvider>
+        <Router />
+      </PostsContextProvider>
     </BrowserRouter>
   )
 }
